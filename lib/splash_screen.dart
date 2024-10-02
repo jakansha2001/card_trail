bf3f1b50a88dfecbe64279b1fee70409e6f1c80c
jakansha2001 <jakansha2001@gmail.com>
2024-10-03 02:33:11 +0530

    feat: add home screen and separate controller for splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,54 +1,24 @@
+import 'package:card_trail/controllers/splash_conroller.dart';
 import 'package:card_trail/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'home_screen.dart'; 
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  SplashScreenState createState() => SplashScreenState();
-}
-
-class SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this,
-    )..forward();
-
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    );
-
-    // Navigate to HomeScreen after animation
-    Future.delayed(const Duration(seconds: 4), () {
-      Get.off(() => HomeScreen());
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    // Initialize the SplashController
+    final SplashController splashController = Get.put(SplashController());
+
     return Scaffold(
       backgroundColor: AppColors.splashBackground,
       body: Center(
         child: FadeTransition(
-          opacity: _animation,
-          child: Image.asset('assets/images/scapia.png'), 
+          opacity: splashController.animation, // Use the controller's animation
+          child: Image.asset('assets/images/scapia.png'),
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 }
